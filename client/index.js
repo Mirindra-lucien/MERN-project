ea0f8a313d7142e0d1821a5ebf101577d534d868
Mirindra <lantosoamirindra@gmail.com>
2023-08-03 09:01:11 +0300

    make the mvc architecture

--- client/index.js
+++ client/index.js
@@ -2,17 +2,11 @@ import 'react-hot-loader/patch';
 import React from "react";
 import reactDom from "react-dom";
 import App from "./app";
-import {AppContainer} from "react-hot-loader/root";
 
 const render = component => {
-    reactDom.render(
-        // <AppContainer>
-            component
-        // </AppContainer>
-        ,
-        document.getElementById("app")
-        );
+    reactDom.render(component,document.getElementById("app"));
 }
+
 if(module.hot) {
     module.hot.accept("./app.js", () => {
         let App = require("./app.js").default;
